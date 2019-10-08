f13d8ed74640fd49c475a2fbcba0c3b52c7386b9
Diego Pronesti <dieh.diego@gmail.com>
2019-10-08 18:48:20 -0300

    keep trying

--- eliminar_escritor.php
+++ eliminar_escritor.php
@@ -7,6 +7,39 @@
     <title>Eliminar Escritor</title>
 </head>
 <body>
-    
+<?php
+ $hostname 	= 'localhost';
+ $database   = 'noticias_bd';
+ $username 	= 'root';
+ $password 	= '';
+ 
+ try {
+     $con = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
+     //print "Conexión exitosa!";
+}
+catch (PDOException $e) {
+     print "¡Error conexion base de datos!: " . $e->getMessage();
+     die();
+} 
+$id_escritor = $_GET["id"];
+$get_escritor = "SELECT * FROM escritores WHERE id=$id_escritor;";
+$escritor = $con->query($get_escritor); 
+foreach ($escritor as $rows) {
+   $nombre_escritor = $rows["apellido"]; 
+   $apellido_escritor = $rows["nombre"]; 
+   $nombre_completo_escritor = $nombre_escritor . " " . $apellido_escritor;
+}
+?>
+    <article><h2>Deseas Eliminar al escritor <?=$nombre_completo_escritor?>?</h2></article>
+    <form action="eliminar_escritor.php?id=<?=$id_escritor?>" method="post">
+    <input type="submit" name="delete" value="Si" /><a href="escritores.php"> Cancelar </a>
+</form>
+<?php
+    if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['delete']))
+    {
+        $delete_escritor_query = "DELETE * FROM escritores WHERE id=$id_escritor;";
+        $delete_escritor = $con->query($delete_escritor_query); 
+    }
+?>
 </body>
 </html>
